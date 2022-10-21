08a9d28a614332fa3ac3232b5b4a83cf4d9a41b4
Andy Jacko <wisdom.mcr@gmail.com>
2022-10-21 11:44:26 +0100

    #1 Add react-router-dom AND Add basic Layout Component/Header Component files...

---
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./HeaderComponent.module.css";
+
+const HeaderComponent = () => {
+  return <div>HeaderComponent</div>;
+};
+
+export default HeaderComponent;
