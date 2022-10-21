7146479c4ef23538f645612f17dda6be52fe432c
JRobbo85 <51794281+JRobbo85@users.noreply.github.com>
2022-10-21 12:22:41 +0100

    #2 Created Home.Js, Home.CSS and added basic layout template divs with basic CSS flex styling

---
+++ src/pages/home/Home.js
@@ -0,0 +1,25 @@
+import './Home.css';
+
+
+const HomeLayout = () => {
+    return (
+    <div id = "homeLayout">
+       
+       
+        <div id = "cardRow">
+            <p>card here</p>
+            <p>card here</p>
+            <p>card here</p>
+        </div>
+
+        <div id = "cardRow">
+            <p>card here</p>
+            <p>card here</p>
+            <p>card here</p>
+        </div>
+        
+    </div>
+    )
+}
+
+export default HomeLayout; 
